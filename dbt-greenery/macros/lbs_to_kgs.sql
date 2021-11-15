88dcb587c343b81ed2ffef4735bcec1420bf3829
Adrian J. Segura <adrian@brooklyndata.co>
2021-11-15 03:03:03 +0000

    fix macro for lbs_to_kgs

--- dbt-greenery/macros/lbs_to_kgs.sql
+++ dbt-greenery/macros/lbs_to_kgs.sql
@@ -1,3 +1,6 @@
-​​{% macro lbs_to_kgs(column_name, precision=2) %}
-   ROUND(({{ column_name }} / 2.205), {{ precision }})
+{% macro lbs_to_kgs(column_name, precision=2) %}
+   round(
+       (COALESCE({{ column_name }}, -99) / 2.205)::numeric, 
+       {{ precision }}
+    )
 {% endmacro %}
